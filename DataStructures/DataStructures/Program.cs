56402a5b448e125d99adca9c855b272b12860340
unknown <Saitharala@gmail.com>
2021-11-21 00:19:37 +0530

    [ Tharala SaiPrashanth] Added Appending Feauture

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -3,35 +3,35 @@
 namespace DataStructures
 {
     class Program
-    {
-        static void Main(string[] args)
         {
-            LinkedList linkedList = new LinkedList();
-            Console.WriteLine("Welcome to Data Structures Programs");
-            bool flag = true;
-            while (flag)
+            static void Main(string[] args)
             {
-                Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Add in Reverse Order \n 3. Exit");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
+                LinkedList linkedList = new LinkedList();
+                Console.WriteLine("Welcome to Data Structures Programs");
+                bool flag = true;
+                while (flag)
                 {
-                    case 1:
-                        linkedList.Add(56);
-                        linkedList.Add(30);
-                        linkedList.Add(70);
-                        break;
-                    case 2:
-                        linkedList.AddInReverseOrder(70);
-                        linkedList.AddInReverseOrder(30);
-                        linkedList.AddInReverseOrder(56);
-                        linkedList.Display();
-                        break;
-                    case 3:
-                        flag = false;
-                        break;
+                    Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Add in Reverse Order \n 3. Exit");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            linkedList.Add(56);
+                            linkedList.Add(30);
+                            linkedList.Add(70);
+                            break;
+                        case 2:
+                            linkedList.AddInReverseOrder(70);
+                            linkedList.AddInReverseOrder(30);
+                            linkedList.AddInReverseOrder(56);
+                            linkedList.Display();
+                            break;
+                        case 3:
+                            flag = false;
+                            break;
+                    }
                 }
-            }
 
+            }
         }
     }
-}
