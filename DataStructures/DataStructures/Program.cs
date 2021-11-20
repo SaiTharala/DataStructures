44fbdc4ad2d21efbb0dd7fa47c27dc27becdd376
unknown <Saitharala@gmail.com>
2021-11-21 00:26:40 +0530

    [ Tharala SaiPrashanth] Added Betwwen Values Feauture

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -5,33 +5,38 @@ namespace DataStructures
     class Program
         {
             static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine("Welcome to Data Structures Programs");
+            bool flag = true;
+            while (flag)
             {
-                LinkedList linkedList = new LinkedList();
-                Console.WriteLine("Welcome to Data Structures Programs");
-                bool flag = true;
-                while (flag)
+                Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Add in Reverse Order \n 3. Insert at Particular Position \n 4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
-                    Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Add in Reverse Order \n 3. Exit");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
-                    {
-                        case 1:
-                            linkedList.Add(56);
-                            linkedList.Add(30);
-                            linkedList.Add(70);
-                            break;
-                        case 2:
-                            linkedList.AddInReverseOrder(70);
-                            linkedList.AddInReverseOrder(30);
-                            linkedList.AddInReverseOrder(56);
-                            linkedList.Display();
-                            break;
-                        case 3:
-                            flag = false;
-                            break;
-                    }
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 2:
+                        linkedList.AddInReverseOrder(70);
+                        linkedList.AddInReverseOrder(30);
+                        linkedList.AddInReverseOrder(56);
+                        linkedList.Display();
+                        break;
+                    case 3:
+                        linkedList.Add(56);
+                        linkedList.Add(70);
+                        linkedList.InsertAtParticularPosition(1, 30);
+                        linkedList.Display();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
                 }
-
             }
         }
     }
+}
