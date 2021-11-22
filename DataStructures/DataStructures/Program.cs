8c492e7f07f2ed2f05543411ecfd5fcd54768738
unknown <Saitharala@gmail.com>
2021-11-22 22:37:36 +0530

    [ Tharala SaiPrashanth] Added  Enqueue-Dequeue Feauture

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -13,7 +13,7 @@ namespace DataStructures
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose an Option : \n 1.Create Simple Linked List \n 2.AddInReverse\n 3.InsertBetween\n 4.RemoveFirstElement\n 5.RemoveLastElement\n 6.Search\n 7.InsertAtParticularNode\n 8.DeleteAtParticularNode\n 9.Exit\n");
+                Console.WriteLine("\nChoose an Option : \n 1.Create Simple Linked List \n 2.AddInReverse\n 3.InsertBetween\n 4.RemoveFirstElement\n 5.RemoveLastElement\n 6.Search\n 7.InsertAtParticularNode\n 8.DeleteAtParticularNode\n 9.StackPush\n 10.StackPop\n 11.Queue\n 12.Dequeue\n 13.Exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -76,6 +76,33 @@ namespace DataStructures
                         linkedList.Display();
                         break;
                     case 9:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Display();
+                        break;
+                    case 10:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.IsEmpty();
+                        stack.Display();
+                        break;
+                    case 11:
+                        queue.Enqueue(70);
+                        queue.Enqueue(30);
+                        queue.Enqueue(56);
+                        queue.Display();
+                        break;
+                    case 12:
+                        queue.Enqueue(70);
+                        queue.Enqueue(30);
+                        queue.Enqueue(56);
+                        queue.Display();
+                        queue.Dequeue();
+                        queue.Display();
+                        break;
+                    case 13:
                         flag = false;
                         break;
                 }
